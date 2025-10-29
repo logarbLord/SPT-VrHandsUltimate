a31a4f8074c4f35a38f4f9f5aac7306b4099adaf
Logarb <foxymanglemurder@gmail.com>
2025-10-29 01:39:32 -0500

    Update to 4.0, add 4 bundles

---
+++ user/mods/VrHands Ultimate/VrHands.cs
@@ -0,0 +1,34 @@
+﻿using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Models.Spt.Mod;
+using SPTarkov.Server.Core.Models.Utils;
+
+namespace _12Bundle
+{
+    public record ModMetadata : AbstractModMetadata
+    {
+        public override string Name { get; init; } = "VRHands Ultimate";
+        public override string Author { get; init; } = "Dar'Zhar";
+        public override List<string>? Contributors { get; init; }
+        public override SemanticVersioning.Version Version { get; init; } = new("1.2.0");
+        public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0");
+        
+        
+        public override List<string>? Incompatibilities { get; init; }
+        public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
+        public override string? Url { get; init; } = "https://forge.sp-tarkov.com/mod/1960/vrhands-ultimate";
+        public override bool? IsBundleMod { get; init; } = true;
+        public override string? License { get; init; } = "MIT";
+        public override string ModGuid { get; init; } = "com.darzhar.vrhandsultimate";
+    }
+
+    [Injectable(TypePriority = OnLoadOrder.PostSptModLoader)]
+    public class BundleExample(ISptLogger<BundleExample> logger) : IOnLoad
+    {
+        public Task OnLoad()
+        {
+            logger.Success("Dar'Zhar's VrHands Ultimate loaded");
+            return Task.CompletedTask;
+        }
+    }
+}
